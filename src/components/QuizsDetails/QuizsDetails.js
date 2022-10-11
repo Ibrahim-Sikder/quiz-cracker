35502a07f6da57d38980b0497345300f5eec1ce7
freelancer-ibrahim <ibrahimfr4450@gmail.com>
2022-10-11 19:11:54 +0600

    Rechart-added

--- src/components/QuizsDetails/QuizsDetails.js
+++ src/components/QuizsDetails/QuizsDetails.js
@@ -3,7 +3,7 @@ import { useLoaderData } from 'react-router-dom';
 
 const QuizsDetails = () => {
     const quizes = useLoaderData()
-    console.log(quizes)
+    
     return (
         <div>
             <h2>All quize </h2>
--- src/components/Statics/Static.js
+++ src/components/Statics/Static.js
@@ -1,11 +1,68 @@
 import React from 'react';
-
+import './Statics.css';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 const Static = () => {
+
+    const data = [
+        {
+          name: 'Page A',
+          uv: 4000,
+          pv: 2400,
+          amt: 2400,
+        },
+        {
+          name: 'Page B',
+          uv: 3000,
+          pv: 1398,
+          amt: 2210,
+        },
+        {
+          name: 'Page C',
+          uv: 2000,
+          pv: 9800,
+          amt: 2290,
+        },
+        {
+          name: 'Page D',
+          uv: 2780,
+          pv: 3908,
+          amt: 2000,
+        },
+        {
+          name: 'Page E',
+          uv: 1890,
+          pv: 4800,
+          amt: 2181,
+        },
+        {
+          name: 'Page F',
+          uv: 2390,
+          pv: 3800,
+          amt: 2500,
+        },
+        {
+          name: 'Page G',
+          uv: 3490,
+          pv: 4300,
+          amt: 2100,
+        },
+      ];
+
+      
     return (
-        <div>
-            <h2>Statics page 
-                
-            </h2>
+        <div className='barChart'>
+            <BarChart
+             width={500}
+             height={300}
+             data={data}
+            >
+                <XAxis dataKey="name" />
+                <YAxis datakey='uv'/>
+                <Tooltip />
+                <Legend />
+          <Bar dataKey="pv" stackId="a" fill="#8884d8" />
+          <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
+            </BarChart>
         </div>
     );
 };
